63cdff7838b1dece076b39d0699b4565f1467ea4
bbou <bbou@ac-toulouse.fr>
2020-01-25 10:24:51 +0100

    tag count

--- pipeline-verb.sh
+++ pipeline-verb.sh
@@ -2,6 +2,7 @@
 
 for f in $*; do
 	./transform.sh add-sensekey.xsl - $f |
+	./transform.sh add-tag_count.xsl - - |
 	./transform.sh add-verb_frame-entity.xsl - - |
 	./transform.sh add-verb_frame.xsl - - |
 	./transform.sh add-verb_frame-attr.xsl - - |
