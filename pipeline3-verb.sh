5db7d7c52bfa6ba23b09e37528950c22e4bcc32c
1313ou <1313ou@gmail.com>
2020-02-13 06:23:28 +0100

    Bypass insert vframe/vtemplate entity

--- pipeline3-verb.sh
+++ pipeline3-verb.sh
@@ -3,10 +3,10 @@
 for f in $*; do
 	./transform3.sh add-sensekey.xsl - $f |
 	./transform3.sh add-tag_count.xsl - - |
-	./transform3.sh add-verb_frame-entity.xsl - - |
+	./transform3.sh add-verb_frame-decl.xsl - - |
 	./transform3.sh add-verb_frame.xsl - - |
 	./transform3.sh add-verb_frame-attr.xsl - - |
-	./transform3.sh add-verb_template-entity.xsl - - |
+	./transform3.sh add-verb_template-decl.xsl - - |
 	./transform3.sh add-verb_template-attr.xsl - - |
 	./transform3.sh remove-syntacticbehaviourref.xsl - -
 done
