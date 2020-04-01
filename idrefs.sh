07c31c433a78cca7db16120a06da60e2dd38a03f
bbou <1313ou@gmail.com>
2020-04-01 06:06:55 +0200

    rebuild script

--- idrefs.sh
+++ idrefs.sh
@@ -10,15 +10,11 @@ M='\u001b[35m'
 C='\u001b[36m'
 Z='\u001b[0m'
 
-#DEF_XPATH="//Synset/@id"
-DEF_XPATH="$1"
-shift
-
 #REF_XPATH="//SynsetRelation[@relType='hypernym']/@target"
+
 REF_XPATH="$1"
 shift
-
-echo -e "${C}DEF:  $DEF_XPATH${Z}" 1>&2;
 echo -e "${C}REF:  $REF_XPATH${Z}" 1>&2;
+
 echo -e "${M}ARGS: $*${Z}" 1>&2;
-java -cp validator2-runnable.jar validator.IDREFValidator "$DEF_XPATH" "$REF_XPATH" $*
+java -cp validator2-runnable.jar validator.IDREFValidator "$REF_XPATH" $*
