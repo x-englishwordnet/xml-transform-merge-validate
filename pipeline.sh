e0c0e1abe9afdfabbe5bc1860763eb9961acb151
bbou <1313ou@gmail.com>
2020-04-08 17:19:26 +0200

    fix name

--- pipeline.sh
+++ pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for f in $*; do
-	./transform.sh add-order.xsl - $f |
+	./transform.sh add-sense_order.xsl - $f |
 	./transform.sh add-sensekey.xsl - - |
 	./transform.sh add-tag_count.xsl - -
 done
