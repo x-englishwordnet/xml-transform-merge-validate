7522f7c192f85a06066e98ec33a0974a12729b53
bbou <1313ou@gmail.com>
2020-04-08 17:06:04 +0200

    add ordering to pipeline

--- pipeline-adj.sh
+++ pipeline-adj.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for f in $*; do
-	./transform.sh add-adj_head.xsl - $f |
+	./transform.sh add-order.xsl - $f |
+	./transform.sh add-adj_head.xsl - - |
 	./transform.sh add-sensekey.xsl - - |
 	./transform.sh add-tag_count.xsl - - |
 	./transform.sh add-adj_position.xsl - -
